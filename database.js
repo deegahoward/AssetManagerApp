4c5c8ecab44ba82bf0acae9e6395b57e0b7bbf47
Deita Howard <deita-grace@live.co.uk>
2016-08-29 19:30:58 +0100

    delete asset functionality implemented

--- database.js
+++ database.js
@@ -1,4 +1,3 @@
-
 var pg = require("pg");
 
 var conString = "pg://postgres:postgres@localhost:5432/postgres";
@@ -7,14 +6,15 @@ var client = new pg.Client(conString);
 client.connect();
 
 
- client.query("CREATE TABLE IF NOT EXISTS assets(name varchar(64), type varchar(64), quantity int)");
- client.query("INSERT INTO assets(name, type, quantity) values($1, $2, $3)", ['Car', 'Vehicle', '4']);
+client.query("CREATE TABLE IF NOT EXISTS assets(name varchar(64) NOT NULL, type varchar(64) NOT NULL, quantity int NOT NULL)");
+client.query("INSERT INTO assets(name, type, quantity) values($1, $2, $3)", ['Car', 'Vehicle', '4']);
+//client.query("ALTER TABLE assets ADD column_name datatype");
 
- var query = client.query("SELECT name, type, quantity FROM assets ORDER BY name");
- query.on("row", function (row, result) {
- result.addRow(row);
- });
- query.on("end", function (result) {
- console.log(JSON.stringify(result.rows, null, "    "));
- client.end();
- });
+var query = client.query("SELECT name, type, quantity FROM assets ORDER BY name");
+query.on("row", function (row, result) {
+    result.addRow(row);
+});
+query.on("end", function (result) {
+    console.log(JSON.stringify(result.rows, null, "    "));
+    client.end();
+});
--- public/app/controllers/mainCtrl.js
+++ public/app/controllers/mainCtrl.js
@@ -1,18 +1,18 @@
 //------------------------- Overall Controller for Authentication methods ------------------------
 
-//doLogin/doLogout methods from https://www.udemy.com/ realtime-meanstack/
 
 var mainApp = angular.module('mainCtrl', ['mainService', 'ui.router']);
 
 
 mainApp.controller('MainController', function ($rootScope, $location, $scope, $state, Assets) {
 
+
     $scope.title = "Asset Manager";
 
     var vm = this;
 
-    $scope.sortType     = 'name';
-    $scope.sortReverse  = false;
+    $scope.sortType = 'name';
+    $scope.sortReverse = false;
     $scope.editorEnabled = false;
 
 
@@ -23,56 +23,52 @@ mainApp.controller('MainController', function ($rootScope, $location, $scope, $s
         });
 
 
-    $scope.changeSort = function(selectedSort){
-
+    $scope.changeSort = function (selectedSort) {
         $scope.sortType = selectedSort;
-
+        if($scope.sortReverse == true){
+            $scope.sortReverse = false;
+        }
+        else {
+            $scope.sortReverse = true;
+        }
     };
 
 
     $scope.addAsset = function () {
-
         Assets.create(vm.newAsset)
-        .then(function (response) {
-            vm.newAsset = {};
-            vm.message = response.data.message;
-
-            console.log(response);
-
-        })
-    };
-
-    $scope.deleteAsset = function(id, index){
-
-        console.log(id);
-        Assets.delete(id)
-            .success(function(){
-
-                $scope.Assets.splice(index, 1);
-
+            .then(function (response) {
+                vm.newAsset = {};
+                vm.message = response.data.message;
+                console.log(response);
             })
     };
 
-    $scope.editAsset = function(asset) {
+    $scope.deleteAsset = function (id, index) {
+        console.log(id);
+        Assets.delete(id)
+            .success(function () {
+                $scope.Assets.splice(index, 1);
+            })
+    };
+
+    $scope.editAsset = function (asset) {
         console.log(asset);
         asset.edit = true;
         console.log(asset.edit);
     };
 
-    $scope.disableEditor = function(asset) {
+    $scope.disableEditor = function (asset) {
         asset.edit = false;
     };
 
-    $scope.save = function(asset) {
+    $scope.save = function (asset) {
+        $scope.disableEditor(asset);
         Assets.update(asset)
-            .success(function (data){
-                $scope.disableEditor(asset);
+        .success(function (data) {
                 console.log(data);
 
             })
-
     };
-
 
 });
 
